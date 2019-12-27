2074d50f7caa11679e878eefc14900187d8d95f3
Harpreetpreet <preetx025@gmail.com>
2019-12-27 09:11:26 -0800

    add footer in contact

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -72,6 +72,9 @@ const Feature = () => (
           <div className="card-image photo2">
             <img src="/images/gallery_item_5.jpg" alt="content" />
           </div>
+          <div className="card-image photo2">
+            <img src="/images/iSweetPhoto-38-XL.jpg" alt="content" />
+          </div>
         </div>
 
         <div className="column">
